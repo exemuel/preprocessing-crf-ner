0587a37b97041b71a8e6ccdcc8e11cea38a5fc58
Samuel Situmeang <samsitumeang@gmail.com>
2022-04-05 12:31:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ Named Entity Recognition based on Conditional Random Field in Indonesian Text.
 
 # standard library
 import sys
+from tabnanny import verbose
 
 # 3rd party packages
 from sklearn_crfsuite import CRF
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 # custom modules
 from util import *
@@ -38,27 +39,32 @@ def main():
         sys.exit()
     
     dp = DatasetPreparator(df_raw, pretrained_tagger)
-    dp = dp.check_post()
-
-    getter = SentenceGetter(dp)
+    df = dp.check_post()
+    df = dp.add_bio_ne()
+    
+    getter = SentenceGetter(df)
     sentences = getter.sentences
 
     X = [sent2features(s) for s in sentences]
     y = [sent2labels(s) for s in sentences]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
-    crf = CRF(algorithm='lbfgs',
-              c1=0.1,
-              c2=0.1,
-              max_iterations=100,
-              all_possible_transitions=True)
+    classes = np.unique(df[["bio_ne"]])
+    classes = classes.tolist()
+    classes.remove("O")
+    new_classes = classes.copy()
+
+    crf = CRF(
+        algorithm='lbfgs',
+        c1=0.1,
+        c2=0.1,
+        max_iterations=100,
+        all_possible_transitions=True,
+        verbose=False
+    )
     crf.fit(X_train, y_train)
 
     y_pred = crf.predict(X_test)
-    classes = np.unique(df_raw[["ne"]])
-    classes = classes.tolist()
-    new_classes = classes.copy()
-    
     y_test_flat = [item for sublist in y_test for item in sublist ]
     y_pred_flat = [item for sublist in y_pred for item in sublist ]
     print(classification_report(y_test_flat, y_pred_flat, labels = new_classes))
