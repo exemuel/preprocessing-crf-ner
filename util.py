df7b8308dc3d71e7cf3f8f129d23cce16c0a80ff
Samuel Situmeang <samsitumeang@gmail.com>
2022-04-19 18:52:14 +0700

    Update util.py

--- util.py
+++ util.py
@@ -9,8 +9,7 @@ import os
 import csv
 import re
 import json
-from datetime import datetime
-from tkinter import Y
+import string
 
 # 3rd party packages
 import pandas as pd
@@ -277,44 +276,83 @@ class SentenceGetter(object):
 # Feature set
 def word2features(sent, i):
     word = sent[i][0]
-    postag = sent[i][1]
 
     features = {
         'bias': 1.0,
-        'word.lower()': word.lower(),
+        'word': word,
+        'len(word)': len(word),
+        'word[:4]': word[:4],
+        'word[:3]': word[:3],
+        'word[:2]': word[:2],
         'word[-3:]': word[-3:],
         'word[-2:]': word[-2:],
-        'word.isupper()': word.isupper(),
-        'word.istitle()': word.istitle(),
+        'word[-4:]': word[-4:],
+        'word.lower()': word.lower(),
+        'word.stemmed': re.sub(r'(.{2,}?)([aeiougyn]+$)',r'\1', word.lower()),
+        'word.ispunctuation': (word in string.punctuation),
         'word.isdigit()': word.isdigit(),
-        'postag': postag,
-        'postag[:2]': postag[:2],
     }
     if i > 0:
         word1 = sent[i-1][0]
-        postag1 = sent[i-1][1]
         features.update({
+            '-1:word': word1,
+            '-1:len(word)': len(word1),
             '-1:word.lower()': word1.lower(),
-            '-1:word.istitle()': word1.istitle(),
-            '-1:word.isupper()': word1.isupper(),
-            '-1:postag': postag1,
-            '-1:postag[:2]': postag1[:2],
+            '-1:word.stemmed': re.sub(r'(.{2,}?)([aeiougyn]+$)',r'\1', word1.lower()),
+            '-1:word[:3]': word1[:3],
+            '-1:word[:2]': word1[:2],
+            '-1:word[-3:]': word1[-3:],
+            '-1:word[-2:]': word1[-2:],
+            '-1:word.isdigit()': word1.isdigit(),
+            '-1:word.ispunctuation': (word1 in string.punctuation),
         })
     else:
         features['BOS'] = True
 
+    if i > 1:
+        word2 = sent[i-2][0]
+        features.update({
+            '-2:word': word2,
+            '-2:len(word)': len(word2),
+            '-2:word.lower()': word2.lower(),
+            '-2:word[:3]': word2[:3],
+            '-2:word[:2]': word2[:2],
+            '-2:word[-3:]': word2[-3:],
+            '-2:word[-2:]': word2[-2:],
+            '-2:word.isdigit()': word2.isdigit(),
+            '-2:word.ispunctuation': (word2 in string.punctuation),
+        })
+
     if i < len(sent)-1:
         word1 = sent[i+1][0]
-        postag1 = sent[i+1][1]
         features.update({
+            '+1:word': word1,
+            '+1:len(word)': len(word1),
             '+1:word.lower()': word1.lower(),
-            '+1:word.istitle()': word1.istitle(),
-            '+1:word.isupper()': word1.isupper(),
-            '+1:postag': postag1,
-            '+1:postag[:2]': postag1[:2],
+            '+1:word[:3]': word1[:3],
+            '+1:word[:2]': word1[:2],
+            '+1:word[-3:]': word1[-3:],
+            '+1:word[-2:]': word1[-2:],
+            '+1:word.isdigit()': word1.isdigit(),
+            '+1:word.ispunctuation': (word1 in string.punctuation),
         })
+
     else:
         features['EOS'] = True
+    if i < len(sent) - 2:
+        word2 = sent[i+2][0]
+        features.update({
+            '+2:word': word2,
+            '+2:len(word)': len(word2),
+            '+2:word.lower()': word2.lower(),
+            '+2:word.stemmed': re.sub(r'(.{2,}?)([aeiougyn]+$)',r'\1', word2.lower()),
+            '+2:word[:3]': word2[:3],
+            '+2:word[:2]': word2[:2],
+            '+2:word[-3:]': word2[-3:],
+            '+2:word[-2:]': word2[-2:],
+            '+2:word.isdigit()': word2.isdigit(),
+            '+2:word.ispunctuation': (word2 in string.punctuation),
+        })
 
     return features
 
