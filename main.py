4269b80d57257aa6084cbac65ab92b9fc64a6def
Samuel Situmeang <samsitumeang@gmail.com>
2022-04-21 22:03:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import train_test_split
 
 # custom modules
 from util import *
+from benchmarking import *
 
 def main():
     data_path = "data/SINGGALANG.tsv"
@@ -29,47 +30,47 @@ def main():
         print(f"Currently, the given file cannot be processed.")
         sys.exit()
     
+    print(f"--- 1st Experiment ---")
     start_time = datetime.now()
-    pp = Preprocessing(df_raw)
-    df = pp.expand_contractions()
-    df = pp.hyphen_comma_splitting()
-    df = pp.lowercasing()
-    df = pp.stemming()
-    df = pp.number2words()
-
-    df = df_raw.copy()
-    dp = DatasetPreparator(df, pretrained_tagger)
-    df = dp.check_post()
-    df = dp.add_bio_ne()
-    
-    getter = SentenceGetter(df)
-    sentences = getter.sentences
-
-    X = [sent2features(s) for s in sentences]
-    y = [sent2labels(s) for s in sentences]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
-
-    classes = np.unique(df[["bio_ne"]])
-    classes = classes.tolist()
-    classes.remove("O")
-    new_classes = classes.copy()
-
-    crf = CRF(
-        algorithm='lbfgs',
-        c1=0.1,
-        c2=0.1,
-        max_iterations=100,
-        all_possible_transitions=True,
-        verbose=False
-    )
-    crf.fit(X_train, y_train)
-
-    y_pred = crf.predict(X_test)
-    y_test_flat = [item for sublist in y_test for item in sublist ]
-    y_pred_flat = [item for sublist in y_pred for item in sublist ]
-    print(classification_report(y_test_flat, y_pred_flat, labels = new_classes))
+    exp1(df_raw, pretrained_tagger)
     end_time = datetime.now()
     print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
+
+    print(f"--- 2nd Experiment ---")
+    start_time = datetime.now()
+    exp2(df_raw, pretrained_tagger)
+    end_time = datetime.now()
+    print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
+
+    print(f"--- 3rd Experiment ---")
+    start_time = datetime.now()
+    exp3(df_raw, pretrained_tagger)
+    end_time = datetime.now()
+    print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
+
+    print(f"--- 4th Experiment ---")
+    start_time = datetime.now()
+    exp4(df_raw, pretrained_tagger)
+    end_time = datetime.now()
+    print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
+
+    print(f"--- 5th Experiment ---")
+    start_time = datetime.now()
+    exp5(df_raw, pretrained_tagger)
+    end_time = datetime.now()
+    print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
+    
+    print(f"--- 6th Experiment ---")
+    start_time = datetime.now()
+    exp6(df_raw, pretrained_tagger)
+    end_time = datetime.now()
+    print("--- %s seconds ---" % (end_time - start_time))
+    print(f"\n")
 
 if __name__ == "__main__":
     main()
